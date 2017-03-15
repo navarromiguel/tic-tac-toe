c2f8359a1039305a13a8c88af4da2dd3771dbcbc
mikelote <migue93@gmail.com>
2017-03-15 15:53:26 +0100

    paso5

--- src/index.js
+++ src/index.js
@@ -46,7 +46,8 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     };
   }
 
@@ -63,13 +64,21 @@ class Game extends React.Component {
     	history: history.concat([{
           squares: squares
         }]),
-    	xIsNext: !this.state.xIsNext
+    	xIsNext: !this.state.xIsNext,
+    	stepNumber: history.length
     });
   }
 
+  jumpTo(step) {
+	  this.setState({
+	    stepNumber: step,
+	    xIsNext: (step % 2) ? false : true,
+	  });
+	}
+
   render() {
   	const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     let status;
     if (winner) {
@@ -77,6 +86,16 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+    const moves = history.map((step, move) => {
+	  const desc = move ?
+	    'Move #' + move :
+	    'Game start';
+	  return (
+	    <li key={move}>
+	      <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+	    </li>
+	  );
+	});
     return (
       <div className="game">
         <div className="game-board">
@@ -84,7 +103,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
